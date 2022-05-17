160edbe922b69cdac22aebc655d8ceaa3a1bd22b
msphanith <sophanith.h61@rsu.ac.th>
2022-05-17 15:01:32 +0700

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import Head from 'next/head';
+import Constants from '../constant';
 
 function MyApp({ Component, pageProps }) {
+  const link = Constants.favicon;
   return (
     <div>
       <Head>
+        <link rel="shortcut icon" href={link.url} type="image/x-icon" />
         <meta name="description" content="This is my portfolio." />
         <title>Jay - Portfolio</title>
       </Head>
